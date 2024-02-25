bcf9d2283abfb057f8a76814d3ca702941eef6a4
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-02-25 01:30:19 -0300

    enviando a primeira versão antes de falar com o monitor

---
+++ Dia 3/dia3.js
@@ -0,0 +1,7 @@
+let NomeUsuario = prompt("QUAL O SEU NOME?")
+let IdadeUsuario = Number(prompt("QUAL A SUA IDADE?"))
+let AlturaUsuario = Number(prompt("QUAL A SUA ALTURA?")) 
+let PesoUsuario = Number(prompt("QUAL O SEU PESO?"))
+let resultadoNascimento = (2024 - IdadeUsuario)
+let resultadoIMC = (PesoUsuario / (AlturaUsuario * AlturaUsuario))
+console.log ("Olá " + NomeUsuario +", você tem "+ IdadeUsuario +" anos, nasceu em "+resultadoNascimento+", tem "+AlturaUsuario+" de altura, pesa "+PesoUsuario+"kg e seu IMC é "+resultadoIMC+"Kg/m2")
