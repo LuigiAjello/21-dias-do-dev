198a9707f67e7c2b8a74be65b8715135b1d4527c
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-02-26 21:47:13 -0300

    Falei com o monitor, tudo certo!Depois volte para fazer comentarios

--- Dia 3/dia3.js
+++ Dia 3/dia3.js
@@ -5,3 +5,4 @@ let PesoUsuario = Number(prompt("QUAL O SEU PESO?"))
 let resultadoNascimento = (2024 - IdadeUsuario)
 let resultadoIMC = (PesoUsuario / (AlturaUsuario * AlturaUsuario))
 console.log ("Olá " + NomeUsuario +", você tem "+ IdadeUsuario +" anos, nasceu em "+resultadoNascimento+", tem "+AlturaUsuario+" de altura, pesa "+PesoUsuario+"kg e seu IMC é "+resultadoIMC+"Kg/m2")
+//teste 
