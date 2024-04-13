1f49f552ffbf884af5aa81835b6668e94398cf31
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-04-13 19:26:24 -0300

    Enviando final

--- Dia 11/exerc2.js
+++ Dia 11/exerc2.js
@@ -19,10 +19,17 @@ if(confirmacao === "S"){
 for(i = 0 ;i < contador ;i++){
     console.log("CARRO"+(i+1)+"= Modelo: "+arraymodelos[i]+"; Valor: "+arrayvalores[i]+"; ano : "+ arrayanos[i]);
 }
-for(i = 0 ;i < contador ;i++){
- 
+for (let i = 0; i < contador - 1; i++) {
+    for (let j = 0; j < contador - i - 1; j++) {
+        
+        if (arrayvalores[j] > arrayvalores[j + 1]) {
+            [arrayvalores[j], arrayvalores[j + 1]] = [arrayvalores[j + 1], arrayvalores[j]];
+            [arraymodelos[j], arraymodelos[j + 1]] = [arraymodelos[j + 1], arraymodelos[j]];
+            [arrayanos[j], arrayanos[j + 1]] = [arrayanos[j + 1], arrayanos[j]];
+        }
+    }
 }
+for (let i = 0; i < contador; i++) {
+    console.log("CARRO " + (i + 1) + ": Modelo - " + arraymodelos[i] + ", Valor - " + arrayvalores[i] + ", Ano - " + arrayanos[i]);
 
-
-
-
+}                                                                                               
