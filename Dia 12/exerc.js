2c671f7d0cfe625bb6bd6b5d4e88642494f54b8d
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-04-15 16:48:36 -0300

    enviando para windows

---
+++ Dia 12/exerc.js
@@ -0,0 +1,26 @@
+
+let respostaum 
+arraynomes= []
+arraysenhas= []
+ confirmacao = true
+ 
+while(confirmacao){
+respostaum = Number(prompt("Escolha 1 para cadastrar, 2 para fazer login, 3 para excluir um cadastro e 4 para encerrar"))
+switch (respostaum) {
+    case  1 :
+        arraynomes 
+        continue;
+    case 2 : 
+        
+        break;
+    case 3: 
+        
+        break;
+    case 4: 
+        
+        break;
+    default:
+    alert("Erro")
+        continue
+}
+}
