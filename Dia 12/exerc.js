7067d126d2312788559c5fbc2d657a3b34a86ad0
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-04-15 21:21:51 -0300

    Melhorei um pouco, falta case 3

--- Dia 12/exerc.js
+++ Dia 12/exerc.js
@@ -1,26 +1,61 @@
 
-let respostaum 
 arraynomes= []
 arraysenhas= []
- confirmacao = true
- 
+let respostaum 
+let i = 0
+let nomelogin 
+let senhalogin 
+confirmacao = true
+
 while(confirmacao){
 respostaum = Number(prompt("Escolha 1 para cadastrar, 2 para fazer login, 3 para excluir um cadastro e 4 para encerrar"))
 switch (respostaum) {
     case  1 :
-        arraynomes 
-        continue;
-    case 2 : 
-        
+        arraynomes[i]= prompt("Digite seu nome : ")
+        arraysenhas[i]= prompt("Digite sua senha : ")
+        i++
         break;
+    case 2 : 
+        nomelogin  = prompt("Digite seu nome : ")
+        senhalogin = prompt("Digite sua senha : ")
+        let loginSucesso = false;
+       for(let j=0;j<i;j++){
+        if(nomelogin === arraynomes[j] && senhalogin === arraysenhas[j]){
+            alert("Login bem-sucedido!")
+            confirmacao = false
+            loginSucesso = true;
+            break;
+        }
+    } 
+     if (!loginSucesso) {
+        alert("Nome de usuário ou senha incorretos.");
+    }
+
+        break
     case 3: 
-        
+    nomelogin  = prompt("Digite seu nome : ")
+    senhalogin = prompt("Digite sua senha : ")
+     eliminarSucesso = false;
+   for(let l=0;l<i;l++){
+    if(nomelogin === arraynomes[l] && senhalogin === arraysenhas[l]){
+       arraynomes [l] = 0 
+       arraysenhas [l] = 0
+        confirmacao = false
+        eliminarSucessoSucesso = true;
+        break;
+    }
+} 
+ if (!eliminarSucessoSucesso) {
+    alert("erro ao eliminar");
+}
+
         break;
     case 4: 
-        
-        break;
+           
+        confirmacao = false
+            break;
     default:
     alert("Erro")
-        continue
+       break;
 }
 }
