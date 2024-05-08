eced60423dc2f4c052421aa1383b1147d4b8a08a
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-05-08 18:03:36 -0300

    avancando

--- Dia 17/DIa17.js
+++ Dia 17/DIa17.js
@@ -6,8 +6,8 @@ class Hotel {
     categoria 
     endereco 
     telefone 
-    constructor (id, nome ,categoria, endereco,telefone){
-        this.id = id 
+    constructor (idHotel, nome ,categoria, endereco,telefone){
+        this.id = idHotel
         this.nome = nome
         this.categoria = categoria 
         this.endereco = endereco
@@ -20,8 +20,8 @@ class Reserva {
      nomeResponsavel
      Diaentrada
      DiaSaida
-    constructor( id,idHotel ,nomeResponsavel,Diaentrada,DiaSaida){
-    this.id = id
+    constructor( idReserva,idHotel ,nomeResponsavel,Diaentrada,DiaSaida){
+    this.id = idReserva
     this.idHotel =idHotel 
     this.nomeResponsavel = nomeResponsavel
     this.Diaentrada=Diaentrada
@@ -29,27 +29,67 @@ class Reserva {
     }
 }
 function CadastroHotel() {
-    let id = Number(prompt("Id do Hotel: "))
+    let idHotel = Number(prompt("Id do Hotel: "))
     let nome =prompt("Nome do Hotel: ")
     let categoria =prompt("categoria do Hotel: ")
     let endereco = prompt("endereço do Hotel: ")
     let telefone= Number(prompt("Numero do Hotel: "))
-    let NovoHotel = new Hotel (id, nome ,categoria, endereco,telefone)
-    ArrayDeHoteis.push(NovoHotel)
-}
-function CadastroHotel() {
-    let id = Number(prompt("Id do Hotel: "))
-    let nome =prompt("Nome do Hotel: ")
-    let categoria =prompt("categoria do Hotel: ")
-    let endereco = prompt("endereço do Hotel: ")
-    let telefone= Number(prompt("Numero do Hotel: "))
-    let NovoHotel = new Hotel (id, nome ,categoria, endereco,telefone)
+    let NovoHotel = new Hotel (idHotel, nome ,categoria, endereco,telefone)
     ArrayDeHoteis.push(NovoHotel)
 }
 function CadastroReserva() {
-    let id = Number(prompt("Id da reserva: "))
+    let idReserva = Number(prompt("Id da reserva: "))
     let idHotel  = Number(prompt("Id do Hotel: "))
     let nomeResponsavel =prompt("endereço do Hotel: ")
     let Diaentrada = Number(prompt("Dia de Entrada: "))
     let DiaSaida = Number(prompt("Dia de saida: "))
+    let NovaReserva = new Reserva (idReserva,idHotel ,nomeResponsavel,Diaentrada,DiaSaida)
+    ArrayDeReservas.push(NovaReserva)
 }
+function exibeReserva() {
+    let idReserva = Number(prompt("Digite o ID da reserva:"));
+    let reservaEncontrada = ArrayDeReservas.find(reserva => reserva.id === idReserva);
+
+    if(reservaEncontrada) {
+        console.log("Detalhes da Reserva:");
+        console.log("ID da Reserva:", reservaEncontrada.id);
+        console.log("ID do Hotel:", reservaEncontrada.idHotel);
+        console.log("Nome do Responsável:", reservaEncontrada.nomeResponsavel);
+        console.log("Dia de Entrada:", reservaEncontrada.Diaentrada);
+        console.log("Dia de Saída:", reservaEncontrada.DiaSaida);
+    } else {
+        console.log("Reserva não encontrada para o ID:", idReserva);
+    }
+}
+function NomeReservas() {
+    
+let NomeRes = prompt('Digite o nome da reserva ')
+let reservasEncontradas = ArrayDeReservas.filter(reservas => reservas.nomeResponsavel=== NomeRes)
+if (reservasEncontradas)
+    console.log("Reservas encontradas para o responsável:", NomeRes)
+reservasEncontradas.forEach(reserva => {
+    console.log("RESERVA:");
+    console.log("ID da Reserva:",reserva.idReserva);
+    console.log("ID do Hotel:",reserva.idHotel);
+    console.log("Nome do Responsável:",reserva.nomeResponsavel);
+    console.log("Dia de Entrada:",reserva.Diaentrada);
+    console.log("Dia de Saída:",reserva.DiaSaida);
+    console.log("#################################");
+});
+}
+function CategoriasHoteis() {
+    
+    let cathotel= prompt('Digite a categoria de hotel :  ')
+    let Hotelencontrados  = ArrayDeReservas.filter(hotel => hotel.categoria=== cathotel)
+    if (Hotelencontrados)
+        console.log("Reservas encontradas para o responsável:", NomeRes)
+    Hotelencontrados.forEach(reserva => {
+        console.log();
+        console.log("ID da Reserva:",reserva.idReserva);
+        console.log("ID do Hotel:",reserva.idHotel);
+        console.log("Nome do Responsável:",reserva.nomeResponsavel);
+        console.log("Dia de Entrada:",reserva.Diaentrada);
+        console.log("Dia de Saída:",reserva.DiaSaida);
+        console.log("#################################");
+    });
+    }
