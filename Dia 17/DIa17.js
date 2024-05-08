aba61f8eba89cec423e33f5c2fe052bf85ea174c
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-05-08 09:00:41 -0300

    terminar funcao de cadastro

--- Dia 17/DIa17.js
+++ Dia 17/DIa17.js
@@ -1,3 +1,5 @@
+let ArrayDeHoteis = []
+let ArrayDeReservas = []
 class Hotel {
     id 
     nome 
@@ -19,9 +21,35 @@ class Reserva {
      Diaentrada
      DiaSaida
     constructor( id,idHotel ,nomeResponsavel,Diaentrada,DiaSaida){
-        
+    this.id = id
+    this.idHotel =idHotel 
+    this.nomeResponsavel = nomeResponsavel
+    this.Diaentrada=Diaentrada
+    this.DiaSaida = DiaSaida
     }
 }
-
-ArrayDeHoteis = []
-ArrayDeReservas = []
+function CadastroHotel() {
+    let id = Number(prompt("Id do Hotel: "))
+    let nome =prompt("Nome do Hotel: ")
+    let categoria =prompt("categoria do Hotel: ")
+    let endereco = prompt("endereço do Hotel: ")
+    let telefone= Number(prompt("Numero do Hotel: "))
+    let NovoHotel = new Hotel (id, nome ,categoria, endereco,telefone)
+    ArrayDeHoteis.push(NovoHotel)
+}
+function CadastroHotel() {
+    let id = Number(prompt("Id do Hotel: "))
+    let nome =prompt("Nome do Hotel: ")
+    let categoria =prompt("categoria do Hotel: ")
+    let endereco = prompt("endereço do Hotel: ")
+    let telefone= Number(prompt("Numero do Hotel: "))
+    let NovoHotel = new Hotel (id, nome ,categoria, endereco,telefone)
+    ArrayDeHoteis.push(NovoHotel)
+}
+function CadastroReserva() {
+    let id = Number(prompt("Id da reserva: "))
+    let idHotel  = Number(prompt("Id do Hotel: "))
+    let nomeResponsavel =prompt("endereço do Hotel: ")
+    let Diaentrada = Number(prompt("Dia de Entrada: "))
+    let DiaSaida = Number(prompt("Dia de saida: "))
+}
