09d2f43d53f84fe3dabdc313c186faf9ecbc571c
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-05-07 22:40:49 -0300

    criei classe hotel e planejei

---
+++ Dia 17/DIa17.js
@@ -0,0 +1,27 @@
+class Hotel {
+    id 
+    nome 
+    categoria 
+    endereco 
+    telefone 
+    constructor (id, nome ,categoria, endereco,telefone){
+        this.id = id 
+        this.nome = nome
+        this.categoria = categoria 
+        this.endereco = endereco
+        this.telefone = telefone
+    }
+}
+class Reserva {
+     id 
+     idHotel 
+     nomeResponsavel
+     Diaentrada
+     DiaSaida
+    constructor( id,idHotel ,nomeResponsavel,Diaentrada,DiaSaida){
+        
+    }
+}
+
+ArrayDeHoteis = []
+ArrayDeReservas = []
