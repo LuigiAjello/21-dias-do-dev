62cf6df57f89530c59c9e65568814fafadc7651a
Luigi Ajello <luigipedrosoajello@gmail.com>
2024-05-09 22:09:46 -0300

    Falta terminar alguns detalhes

--- Dia 17/DIa17.js
+++ Dia 17/DIa17.js
@@ -15,7 +15,7 @@ class Hotel {
     }
 }
 class Reserva {
-     id 
+     idReserva
      idHotel 
      nomeResponsavel
      Diaentrada
@@ -33,14 +33,14 @@ function CadastroHotel() {
     let nome =prompt("Nome do Hotel: ")
     let categoria =prompt("categoria do Hotel: ")
     let endereco = prompt("endereço do Hotel: ")
-    let telefone= Number(prompt("Numero do Hotel: "))
+    let telefone= Number(prompt("Telefone do Hotel: "))
     let NovoHotel = new Hotel (idHotel, nome ,categoria, endereco,telefone)
     ArrayDeHoteis.push(NovoHotel)
 }
 function CadastroReserva() {
     let idReserva = Number(prompt("Id da reserva: "))
     let idHotel  = Number(prompt("Id do Hotel: "))
-    let nomeResponsavel =prompt("endereço do Hotel: ")
+    let nomeResponsavel =prompt("nome do responsavel : ")
     let Diaentrada = Number(prompt("Dia de Entrada: "))
     let DiaSaida = Number(prompt("Dia de saida: "))
     let NovaReserva = new Reserva (idReserva,idHotel ,nomeResponsavel,Diaentrada,DiaSaida)
@@ -50,7 +50,7 @@ function exibeReserva() {
     let idReserva = Number(prompt("Digite o ID da reserva:"));
     let reservaEncontrada = ArrayDeReservas.find(reserva => reserva.id === idReserva);
 
-    if(reservaEncontrada) {
+    if(reservaEncontrada.length > 0) {
         console.log("Detalhes da Reserva:");
         console.log("ID da Reserva:", reservaEncontrada.id);
         console.log("ID do Hotel:", reservaEncontrada.idHotel);
@@ -69,27 +69,30 @@ if (reservasEncontradas)
     console.log("Reservas encontradas para o responsável:", NomeRes)
 reservasEncontradas.forEach(reserva => {
     console.log("RESERVA:");
-    console.log("ID da Reserva:",reserva.idReserva);
+    console.log("ID da Reserva:",reserva.id);
     console.log("ID do Hotel:",reserva.idHotel);
     console.log("Nome do Responsável:",reserva.nomeResponsavel);
     console.log("Dia de Entrada:",reserva.Diaentrada);
     console.log("Dia de Saída:",reserva.DiaSaida);
     console.log("#################################");
-});
+})
 }
 function CategoriasHoteis() {
     
     let cathotel= prompt('Digite a categoria de hotel :  ')
-    let Hotelencontrados  = ArrayDeReservas.filter(hotel => hotel.categoria=== cathotel)
+    let Hotelencontrados  = ArrayDeHoteis.filter(hotel => hotel.categoria=== cathotel)
     if (Hotelencontrados)
-        console.log("Reservas encontradas para o responsável:", NomeRes)
-    Hotelencontrados.forEach(reserva => {
-        console.log();
-        console.log("ID da Reserva:",reserva.idReserva);
-        console.log("ID do Hotel:",reserva.idHotel);
-        console.log("Nome do Responsável:",reserva.nomeResponsavel);
-        console.log("Dia de Entrada:",reserva.Diaentrada);
-        console.log("Dia de Saída:",reserva.DiaSaida);
+        console.log("Categorias de hoteis ", cathotel)
+    Hotelencontrados.forEach(hotel => {
+        console.log("Nome: ", hotel.nome);
+        console.log("ID do Hotel: ",hotel.id);
+        console.log("telefone :",hotel.telefone );
+        console.log("Endereço :", hotel.endereco);
         console.log("#################################");
     });
     }
+    CadastroHotel()
+    CadastroReserva()
+    exibeReserva()
+    NomeReservas()
+CategoriasHoteis() 
